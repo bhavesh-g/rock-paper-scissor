fc552848aebd15d69cc67a0586bd984b4aeddaa4
BHAVESH GUPTA <guptabhavesh6@gmail.com>
2018-09-03 21:14:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,13 +16,13 @@ $("#rock").click(function(event) {
 	a=getRandomInt(1,4);
 	if(a==1){
 		$("#computer").css('backgroundImage', "url('images/rock.gif')");
-		$("#resultline").html('DRAW');
+		$("#resultline").text('DRAW');
 
 	}
 
 	if(a==2){
 		$("#computer").css('backgroundImage', "url('images/paper.png')");
-		$("#resultline").html('YOU LOSE');
+		$("#resultline").text('YOU LOSE');
 		scoreinc=Number($("#compscore").html())+1; 
 		$("#compscore").html(scoreinc); if(scoreinc==10){alert("computer wins!"); setTimeout(function(){location.reload();},1000); }
 		
@@ -31,7 +31,7 @@ $("#rock").click(function(event) {
 
 	if(a==3){
 		$("#computer").css('backgroundImage', "url('images/scissor.gif')");
-		$("#resultline").html('YOU WIN');
+		$("#resultline").text('YOU WIN');
 		scoreinc=Number($("#playerscore").html())+1; 
 		$("#playerscore").html(scoreinc); if(scoreinc==10){alert("you win!"); setTimeout(function(){location.reload();},1000); }
 		
@@ -50,13 +50,13 @@ $("#paper").click(function(event) {
 	if(a==1){
 		$("#computer").css('backgroundImage', "url('images/paper.gif')");
 		
-		$("#resultline").html('DRAW');
+		$("#resultline").text('DRAW');
 
 	}
 
 	if(a==2){
 		$("#computer").css('backgroundImage', "url('images/scissor.png')");
-		$("#resultline").html('YOU LOSE');
+		$("#resultline").text('YOU LOSE');
 		scoreinc=Number($("#compscore").html())+1; 
 		$("#compscore").html(scoreinc); if(scoreinc==10){alert("computer wins!"); setTimeout(function(){location.reload();},1000); }
 		
@@ -66,7 +66,7 @@ $("#paper").click(function(event) {
 	if(a==3){
 		$("#computer").css('backgroundImage', "url('images/rock.gif')");
 		
-		$("#resultline").html('YOU WIN');
+		$("#resultline").text('YOU WIN');
 		scoreinc=Number($("#playerscore").html())+1; 
 		$("#playerscore").html(scoreinc); if(scoreinc==10){alert("you win!"); setTimeout(function(){location.reload();},1000); }
 		
@@ -84,13 +84,13 @@ $("#scissor").click(function(event) {
 	if(a==1){
 		
 		$("#computer").css('backgroundImage', "url('images/scissor.gif')");
-		$("#resultline").html('DRAW');
+		$("#resultline").text('DRAW');
 
 	}
 
 	if(a==2){
 		$("#computer").css('backgroundImage', "url('images/rock.png')");
-		$("#resultline").html('YOU LOSE');
+		$("#resultline").text('YOU LOSE');
 		scoreinc=Number($("#compscore").html())+1; 
 		$("#compscore").html(scoreinc); if(scoreinc==10){alert("computer wins!"); setTimeout(function(){location.reload();},1000); }
 		
@@ -100,7 +100,7 @@ $("#scissor").click(function(event) {
 	if(a==3){
 		
 		$("#computer").css('backgroundImage', "url('images/paper.gif')");
-		$("#resultline").html('YOU WIN');
+		$("#resultline").text('YOU WIN');
 		scoreinc=Number($("#playerscore").html())+1; 
 		$("#playerscore").html(scoreinc); if(scoreinc==10){alert("you win!"); setTimeout(function(){location.reload();},1000); }
 		
